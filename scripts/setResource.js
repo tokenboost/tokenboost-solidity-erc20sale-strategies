f21a0817e42c5f8615a517bd165ca6623c0523c0
Yoonjae Yoo <yoonjae@dnext.co>
2018-10-24 04:05:22 +0900

    Update setResource: check exception.

--- scripts/setResource.js
+++ scripts/setResource.js
@@ -1,9 +1,13 @@
 module.exports = async function (renderer, locale, key, value) {
-    return await new Promise(async (resolve,) => {
-        let prev = await renderer.resource(locale, key);
-        if (prev.toString() !== value.toString()) {
-            await renderer.setResource(locale, key, value);
+    return await new Promise(async (resolve, reject) => {
+        try {
+            let prev = await renderer.resource(locale, key);
+            if (prev.toString() !== value.toString()) {
+                await renderer.setResource(locale, key, value);
+            }
+            resolve();
+        } catch (e) {
+            reject(e);
         }
-        resolve();
     });
 };
